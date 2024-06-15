f1f7b42801f6512ee284590a4f6f7ca05c99b3a1
Zyrephus <wilsonchen315@gmail.com>
2024-06-15 13:12:01 -0400

    Create 0347-top-k-frequent-elements.py

---
+++ Medium/0347-top-k-frequent-elements.py
@@ -0,0 +1,20 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        nums_map = {} # Frequency of numbers
+        freq = [[] for i in range(len(nums) + 1)] # Bucket sort
+
+        # Counting frequency
+        for i in nums:
+            nums_map[i] = 1 + nums_map.get(i, 0)
+
+        # Grouping numbers by their frequency
+        for num, count in nums_map.items():
+            freq[count].append(num)
+
+        res = []
+        # Traverse frequency list from high to low
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
