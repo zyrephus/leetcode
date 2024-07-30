4a21be010d052cefa77e30d3e0b01a3a865f5413
Zyrephus <wilsonchen315@gmail.com>
2024-07-29 20:15:59 -0400

    Update 0013-roman-to-integer.py

--- Easy/0013-roman-to-integer.py
+++ Easy/0013-roman-to-integer.py
@@ -1,19 +1,28 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        lookup = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500, 
-            'M': 1000
-        }
+        # O(n) solution
 
+        # Map of Roman numerals to their integer values
+        symbols = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
+        
         res = 0
-        for i in range(len(s)):
-            if i != len(s) - 1 and lookup[s[i]] < lookup[s[i + 1]]:
-                res -= lookup[s[i]]
-            else:
-                res += lookup[s[i]]
+
+        # Iterate through the string from right to left
+        for i in range(len(s) - 1, -1, -1):
+            curr = symbols[s[i]]
+            
+            # If the current value is less than the next value, subtract it
+            if i + 1 < len(s) and curr < symbols[s[i + 1]]:
+                curr = -curr
+
+            res += curr
+
         return res
