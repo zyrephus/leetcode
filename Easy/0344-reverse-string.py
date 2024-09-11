a0b697286a06ab143da01bf47e3eb3c7b43f2cb9
Zyrephus <wilsonchen315@gmail.com>
2024-09-11 10:34:23 -0400

    Update 0344-reverse-string.py

--- Easy/0344-reverse-string.py
+++ Easy/0344-reverse-string.py
@@ -3,11 +3,10 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        
         # O(n) solution
-        
-        left = 0
-        right = len(s) - 1
+
+        left, right = 0, len(s) - 1
+
         while left <= right:
             s[left], s[right] = s[right], s[left]
             left += 1
