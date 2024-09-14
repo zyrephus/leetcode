d5bb08166d0579227fea3e572305d9ed1188fbc1
Zyrephus <wilsonchen315@gmail.com>
2024-09-14 15:07:01 -0400

    Update 0125-valid-palindrome.py

--- Easy/0125-valid-palindrome.py
+++ Easy/0125-valid-palindrome.py
@@ -2,40 +2,38 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         # O(n) solution
 
-        alphanumeric = set('abcdefghijklmnopqrstuvwxyz1234567890')
+        alphanumeric = set("abcdefghijklmnopqrstuvwxyz1234567890")
 
-        left = 0
-        right = len(s) - 1
-
-        while left <= right:
-            if s[left].lower() not in alphanumeric:
-                left += 1
-                continue
-            if s[right].lower() not in alphanumeric:
-                right -= 1
-                continue
-
-            if s[left].lower() == s[right].lower():
-                left += 1
-                right -= 1
-                continue
-            else:
-                return False
+        cleaned_s = ""
+        for char in s:
+            if char.lower() in alphanumeric:
+                cleaned_s += char.lower()
         
-        return True
-
+        return cleaned_s == cleaned_s[::-1]
 
 # First approach
 # class Solution:
 #     def isPalindrome(self, s: str) -> bool:
-
 #         # O(n) solution
 
 #         alphanumeric = set('abcdefghijklmnopqrstuvwxyz1234567890')
+
+#         left = 0
+#         right = len(s) - 1
+
+#         while left <= right:
+#             if s[left].lower() not in alphanumeric:
+#                 left += 1
+#                 continue
+#             if s[right].lower() not in alphanumeric:
+#                 right -= 1
+#                 continue
+
+#             if s[left].lower() == s[right].lower():
+#                 left += 1
+#                 right -= 1
+#                 continue
+#             else:
+#                 return False
         
-#         cleaned_s = ''
-#         for c in s:
-#             if c.lower() in alphanumeric:
-#                 cleaned_s += c.lower()
-        
-#         return cleaned_s == cleaned_s[::-1]
+#         return True
