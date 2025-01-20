4245a220c799c41271442cf3062b442e27df1385
Wilson <wilsonchen315@gmail.com>
2025-01-20 08:44:45 -0500

    Update 0238-product-of-array-except-self.py

--- Medium/0238-product-of-array-except-self.py
+++ Medium/0238-product-of-array-except-self.py
@@ -1,18 +1,38 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         # O(n) solution
+        
+        res = [1] * len(nums)
 
-        l_mult = 1
-        r_mult = 1
-        n = len(nums)
-        left = [None] * n
-        right = [None] * n
+        left = 1
+        for i in range(len(nums)):
+            res[i] = left
+            left *= nums[i]
 
-        for i in range(n):
-            j = -i - 1
-            left[i] = l_mult
-            right[j] = r_mult
-            l_mult *= nums[i]
-            r_mult *= nums[j]
+        right = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= right
+            right *= nums[i]
+        
+        return res
 
-        return [l * r for l, r in zip(left, right)]
+
+
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         # O(n) solution
+
+#         l_mult = 1
+#         r_mult = 1
+#         n = len(nums)
+#         left = [None] * n
+#         right = [None] * n
+
+#         for i in range(n):
+#             j = -i - 1
+#             left[i] = l_mult
+#             right[j] = r_mult
+#             l_mult *= nums[i]
+#             r_mult *= nums[j]
+
+#         return [l * r for l, r in zip(left, right)]
