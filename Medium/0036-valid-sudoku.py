a9a37cfe248532cbe6f54abfa7827c364e43f6b6
Wilson <wilsonchen315@gmail.com>
2025-02-17 14:31:55 -0500

    Update 0036-valid-sudoku.py

--- Medium/0036-valid-sudoku.py
+++ Medium/0036-valid-sudoku.py
@@ -1,37 +1,23 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        # O(1) solution
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        squares = defaultdict(set) # key = (r // 3, c // 3)
 
-        # Rows
-        for i in range(9):
-            row_set = set()
-            for j in range(9):
-                n = board[i][j]
-                if n in row_set:
+        for r in range(9):
+            for c in range(9):
+                curr = board[r][c]
+
+                if curr == ".":
+                    continue
+
+                if (curr in rows[r] or 
+                curr in cols[c] or 
+                curr in squares[(r // 3, c // 3)]):
                     return False
-                elif n != ".":
-                    row_set.add(n)
-
-        # Columns
-        for i in range(9):
-            col_set = set()
-            for j in range(9):
-                n = board[j][i]
-                if n in col_set:
-                    return False
-                elif n != ".":
-                    col_set.add(n)
-
-        # Sub-boxes
-        for i in range(0, 9, 3):
-            for j in range(0, 9, 3):
-                sub_set = set()
-                for k in range(3):
-                    for l in range(3):
-                        n = board[i + k][j + l]
-                        if n in sub_set:
-                            return False
-                        elif n != ".":
-                            sub_set.add(n)
-        
+                
+                rows[r].add(curr)
+                cols[c].add(curr)
+                squares[(r // 3, c // 3)].add(curr)
+            
         return True
