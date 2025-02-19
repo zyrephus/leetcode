c4408d1b6b366c0a93556d285da133337e2c5980
Wilson <wilsonchen315@gmail.com>
2025-02-19 09:50:03 -0500

    Update 0150-evaluate-reverse-polish-notation.py

--- Medium/0150-evaluate-reverse-polish-notation.py
+++ Medium/0150-evaluate-reverse-polish-notation.py
@@ -1,23 +1,20 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        # O(n) solution
-
-        stack = [] # Using stack approach
+        stack = []
 
         for c in tokens:
-            if c == '+':
+            if c == "+":
                 stack.append(stack.pop() + stack.pop())
-            elif c == '-':
-                # Need to store in variable since the order matters
+            elif c == "-":
                 a, b = stack.pop(), stack.pop()
                 stack.append(b - a)
-            elif c == '*':
+            elif c == "*":
                 stack.append(stack.pop() * stack.pop())
-            elif c == '/':
-                # Same as subtraction
+            elif c == "/":
                 a, b = stack.pop(), stack.pop()
-                stack.append(int(b / a)) # Will round towards 0
+                stack.append(int(float(b) / a))
             else:
-                stack.append(int(c)) # Add to stack if it's not an operator
-        
+                stack.append(int(c))
+            
         return stack[0]
+        
