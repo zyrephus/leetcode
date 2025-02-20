5fd73ced35dce5e0fd91865eba66f242c49e1b32
Wilson <wilsonchen315@gmail.com>
2025-02-20 18:42:08 -0500

    Update 0022.generate-parantheses.py

--- Medium/0022.generate-parantheses.py
+++ Medium/0022.generate-parantheses.py
@@ -1,21 +1,22 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        # I have no idea what the time complexity is and am too lazy to check
+        res, string = [], []
 
-        stack = []
-        res = []
-
-        def backtrack(open_n, closed_n):
-            if open_n == closed_n == n:
-                res.append(''.join(stack)) # Join all () as one element
-            if open_n < n: # Add ( if open < n
-                stack.append('(')
-                backtrack(open_n + 1, closed_n)
-                stack.pop()
-            if closed_n < open_n:         # Add ) if closed < open
-                stack.append(')')
-                backtrack(open_n, closed_n + 1)
-                stack.pop()
+        def backtrack(opn, cls):
+            if len(string) == 2 * n:
+                res.append("".join(string))
+                return
+            
+            if opn < n:
+                string.append("(")
+                backtrack(opn + 1, cls)
+                string.pop()
+            
+            if opn > cls:
+                string.append(")")
+                backtrack(opn, cls + 1)
+                string.pop()
         
         backtrack(0, 0)
+        
         return res
