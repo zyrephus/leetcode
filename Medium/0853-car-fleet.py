7b253bcd40c468b6a1b3ff351c784fa7fb4be575
Wilson <wilsonchen315@gmail.com>
2025-02-26 10:29:24 -0500

    Update 0853-car-fleet.py

--- Medium/0853-car-fleet.py
+++ Medium/0853-car-fleet.py
@@ -1,13 +1,16 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        pair = [[p, s] for p, s in zip(position, speed)]
+        pair = [[p, s] for p, s in zip(position, speed)] # List of [position, speed]
+        pair = sorted(pair, reverse=True) # Sorted in reverse order and exclude first element
         
-        stack = []
-
-        for p, s in sorted(pair)[::-1]:
-            stack.append((target - p) / s)
-
-            if len(stack) > 1 and stack[-1] <= stack[-2]:
-                stack.pop()
+        res = 1
         
-        return len(stack)
+        curr_fleet = (target - pair[0][0]) / pair[0][1]
+        for p, s in pair[1:]:
+            curr_car = (target - p) / s
+
+            if curr_fleet < curr_car:
+                res += 1
+                curr_fleet = curr_car
+
+        return res
