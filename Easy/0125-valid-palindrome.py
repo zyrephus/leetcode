79671ee2a4d934b451955b022cf3e6dca824f77b
Wilson <wilsonchen315@gmail.com>
2025-03-01 09:50:49 -0500

    Update 0125-valid-palindrome.py

--- Easy/0125-valid-palindrome.py
+++ Easy/0125-valid-palindrome.py
@@ -1,39 +1,17 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # O(n) solution
+        l, r = 0, len(s) - 1
 
-        alphanumeric = set("abcdefghijklmnopqrstuvwxyz1234567890")
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while r > l and not s[r].isalnum():
+                r -= 1
+            
+            if s[l].lower() != s[r].lower():
+                return False
 
-        cleaned_s = ""
-        for char in s:
-            if char.lower() in alphanumeric:
-                cleaned_s += char.lower()
+            l += 1
+            r -= 1
         
-        return cleaned_s == cleaned_s[::-1]
-
-# First approach
-# class Solution:
-#     def isPalindrome(self, s: str) -> bool:
-#         # O(n) solution
-
-#         alphanumeric = set('abcdefghijklmnopqrstuvwxyz1234567890')
-
-#         left = 0
-#         right = len(s) - 1
-
-#         while left <= right:
-#             if s[left].lower() not in alphanumeric:
-#                 left += 1
-#                 continue
-#             if s[right].lower() not in alphanumeric:
-#                 right -= 1
-#                 continue
-
-#             if s[left].lower() == s[right].lower():
-#                 left += 1
-#                 right -= 1
-#                 continue
-#             else:
-#                 return False
-        
-#         return True
+        return True
