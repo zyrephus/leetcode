1da7b2d187beb4c3e24bd3b1e623adb9dbb99b3e
Wilson <wilsonchen315@gmail.com>
2025-03-02 12:14:12 -0500

    Update 0015-3sum.py

--- Medium/0015-3sum.py
+++ Medium/0015-3sum.py
@@ -1,35 +1,30 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # O(n^2) solution
- 
         res = []
-        n = len(nums)
 
-        nums.sort() # Sort the list
+        nums.sort()
 
-        for i in range(n):
-            if nums[i] > 0:
-                break
-            # We don't want to consider the same number
-            if i > 0 and nums[i - 1] == nums[i]:
-                continue
+        for i in range(len(nums)):
+            l, r = i + 1, len(nums) - 1
             
-            left, right = i + 1, n - 1
-            while left < right:
-                summ = nums[i] + nums[left] + nums[right]
+            # Skip duplicate values for i
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
 
-                if summ == 0:
-                    res.append([nums[i], nums[left], nums[right]])
-
-                    left, right = left + 1, right - 1
-                    # Skip duplicates for left and right pointers
-                    while left < right and nums[left - 1] == nums[left]:
-                        left += 1
-                    while left < right and nums[right + 1] == nums[right]:
-                        right -= 1
-                elif summ < 0:
-                    left += 1
+            while l < r:
+                threeSum = nums[i] + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
                 else:
-                    right -= 1
+                    res.append([nums[i], nums[l], nums[r]])
+
+                    r -= 1
+                    l += 1
+
+                    # Skip duplicate values for l
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
         
         return res
