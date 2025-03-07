8ec9b7e04a7982637669ea7cc17856b94054ca44
Wilson <wilsonchen315@gmail.com>
2025-03-07 10:42:24 -0500

    Update 0424-longest-repeating-character-replacement.py

--- Medium/0424-longest-repeating-character-replacement.py
+++ Medium/0424-longest-repeating-character-replacement.py
@@ -1,21 +1,17 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        # O(n) solution  
-
-        count = {} # Hashmap to count occurences of letters
+        freq = {}
 
         res = 0
-        
-        # Sliding window
-        left = 0
-        for right in range(len(s)):
-            count[s[right]] = 1 + count.get(s[right], 0) # Update frequency of letters
-            
-            # Shift left if number of operations exceed k
-            if (right - left + 1) - max(count.values()) > k:
-                count[s[left]] -= 1
-                left += 1
 
-            res = max(res, right - left + 1)
+        l = 0
+        for r in range(len(s)):
+            freq[s[r]] = 1 + freq.get(s[r], 0)
+
+            if (r - l + 1) - max(freq.values()) > k:
+                freq[s[l]] -= 1
+                l += 1
+
+            res = max(res, r - l + 1)
 
         return res
