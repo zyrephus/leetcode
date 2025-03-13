1ac74a7f57826bcc568e5835642d5548bf39f953
Wilson <wilsonchen315@gmail.com>
2025-03-13 13:03:46 -0400

    Update 0206-reverse-linked-list.py

--- Easy/0206-reverse-linked-list.py
+++ Easy/0206-reverse-linked-list.py
@@ -3,18 +3,20 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # O(n) solution
+        if not head: 
+            return None
         
-        prev = None 
+        prev = None
         curr = head
-
-        while curr:
-            temp = curr.next
-            curr.next = prev # Reverse curr node pointer to point to prev node
-
-            prev = curr # Move the prev up to curr node
-            curr = temp #  Move curr to the next node in original list
-
+        
+        while curr is not None:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        
         return prev
+        
