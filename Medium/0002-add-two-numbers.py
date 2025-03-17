bf07e1f1c1bb55d186e89d3822d93bc12f784f4a
Wilson <wilsonchen315@gmail.com>
2025-03-17 10:17:05 -0400

    Update 0002-add-two-numbers.py

--- Medium/0002-add-two-numbers.py
+++ Medium/0002-add-two-numbers.py
@@ -5,30 +5,23 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        # O(n) solution, where n is the larger LL
-
-        # Dummy node to serve as the start of the result LL
-        res = ListNode()
-        curr = res
+        dummy = curr = ListNode(0, None)
 
         carry = 0
-        while l1 or l2 or carry != 0:
-            # Getting values of each LL
-            v1 = l1.val if l1 else 0
-            v2 = l2.val if l2 else 0
 
-            # Calculating the new carry and value to be inserted
-            total = v1 + v2 + carry
-            carry = total // 10
-            total = total % 10
-            curr.next = ListNode(total)
-            
-            # Updating nodes
+        while l1 or l2 or carry:
+            val_1 = l1.val if l1 else 0
+            val_2 = l2.val if l2 else 0
+
+            summ = val_1 + val_2 + carry
+            value = summ % 10
+            carry = summ // 10
+
+            curr.next = ListNode(value, None)
             curr = curr.next
-            if l1:
-                l1 = l1.next
-            if l2:
-                l2 = l2.next
+
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
+
+        return dummy.next
         
-        # Returning next of dummy node (head of result list)
-        return res.next
