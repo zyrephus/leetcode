e2018a949475212b03c0a7541c59b51d9e71ad5f
Wilson <wilsonchen315@gmail.com>
2025-04-23 20:22:47 -0400

    Update 0104-maximum-depth-of-binary-tree.py

--- Easy/0104-maximum-depth-of-binary-tree.py
+++ Easy/0104-maximum-depth-of-binary-tree.py
@@ -7,7 +7,8 @@
 
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if root is None:
+        if not root:
             return 0
 
         return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+        
