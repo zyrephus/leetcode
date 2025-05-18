76de9038a01138d9bb4ddc9cca8027dc487971fe
Wilson <wilsonchen315@gmail.com>
2025-05-18 12:41:25 -0400

    Create 0124-binary-tree-maximum-path-sum.py

---
+++ Hard/0124-binary-tree-maximum-path-sum.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res = [root.val]
+
+        def dfs(node):
+            if not node: 
+                return 0
+
+            left_max = dfs(node.left)
+            right_max = dfs(node.right)
+
+            left_max = max(left_max, 0)
+            right_max = max(right_max, 0)
+
+            # Compute max path sum with splitting
+            res[0] = max(res[0], node.val + left_max + right_max)
+
+            return node.val + max(left_max, right_max)
+            
+        dfs(root)
+
+        return res[0]
+
