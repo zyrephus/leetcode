7b709d48ea727ee5c511c9b244139f213e829edd
Wilson <wilsonchen315@gmail.com>
2025-08-20 12:45:09 -0400

    Update 0200-number-of-islands.py

--- Medium/0200-number-of-islands.py
+++ Medium/0200-number-of-islands.py
@@ -1,31 +1,27 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-
         res = 0
-        visited = set()
+
         ROWS, COLS = len(grid), len(grid[0])
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
-        def bfs(r, c):
-            queue = deque([(r, c)])
-            visited.add((r, c))
+        def bfs(sr, sc):
+            q = deque([(sr, sc)])
+            grid[sr][sc] = '0'
 
-            while queue:
-                row, col = queue.popleft()
-
-                for dr, dc in directions:
-                    curr_dr = row + dr
-                    curr_dc = col + dc
-                    if (curr_dr) in range(ROWS) and (curr_dc) in range(COLS) and grid[curr_dr][curr_dc] == "1" and (curr_dr, curr_dc) not in visited:
-                        queue.append((curr_dr, curr_dc))
-                        visited.add((curr_dr, curr_dc))
-
+            while q:
+                r, c = q.popleft()
+                for dr, dc in dirs:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < ROWS and 0 <= nc < COLS and grid[nr][nc] == '1':
+                        grid[nr][nc] = '0'
+                        q.append((nr, nc))
+        
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == "1" and (r, c) not in visited:
-                    bfs(r, c)
+                if grid[r][c] == '1':
                     res += 1
+                    bfs(r, c)
 
         return res
+        
