eeb13fccf803b72f309c054bbb48487af14630e3
Wilson <wilsonchen315@gmail.com>
2025-09-16 19:41:26 -0400

    Create 0207-course-schedule.py

---
+++ Medium/0207-course-schedule.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        preMap = { i: [] for i in range(numCourses) }
+        for course, pre in prerequisites:
+            preMap[course].append(pre)
+
+        visitSet = set()
+        def dfs(course):
+            if course in visitSet:
+                return False
+            if preMap[course] == []:
+                return True
+
+            visitSet.add(course)
+            for pre in preMap[course]:
+                if not dfs(pre): 
+                    return False
+            visitSet.remove(course)
+
+            preMap[course] = []
+                
+            return True
+
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+        
+        return True
+        
